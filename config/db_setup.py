822262022c28833e69258771f8874303c2cdaebf
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-16 23:25:28 -0300

    feat: added docstrings to db_setup methods

--- config/db_setup.py
+++ config/db_setup.py
@@ -5,14 +5,22 @@ import os
 
 __all__ = ['init']
 
-async def init():
-
+async def init() -> None:
+    """
+    Initialize the database connection.
+    """
     config = await retrieve_tortoise_config()
     await Tortoise.init(config)
     await Tortoise.generate_schemas()
     log_info("Database connection established from Tortoise ORM")
 
 async def retrieve_tortoise_config() -> dict:
+    """
+    Retrieve the Tortoise ORM configuration.
+
+    Returns:
+        dict: The Tortoise ORM configuration.
+    """
     load_dotenv()
     credentials = await retrieve_credentials()
     user = credentials['user']
@@ -33,6 +41,12 @@ async def retrieve_tortoise_config() -> dict:
     return config
 
 async def retrieve_credentials() -> dict:
+    """
+    Retrieve the database credentials.
+
+    Returns:
+        dict: The database credentials.
+    """
     load_dotenv()
     user = os.getenv('POSTGRES_USER')
     password = os.getenv('POSTGRES_PASSWORD')
