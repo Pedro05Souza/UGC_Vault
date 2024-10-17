ae8b02394ebcc7fe47efb4d5848c498aa315a4e2
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-16 23:19:19 -0300

    refactor: change integer to big int in id field and added default value for balance

--- models/user.py
+++ models/user.py
@@ -2,9 +2,10 @@ from tortoise.models import Model
 from tortoise import fields
 
 __all__ = ['User']
+
 class User(Model):
-    id = fields.IntField(primary_key=True, default=0)
-    balance = fields.IntField()
+    id = fields.BigIntField(primary_key=True)
+    balance = fields.IntField(default=0)
 
     def __str__(self):
         return f'user_id: {self.id}, balance: {self.balance}'
