4fd0a81125dcc77d652d3a3f2193a11c472b0f01
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-16 23:19:38 -0300

    refactor: UI design remake in slots command

--- core/cogs/economy_commands.py
+++ core/cogs/economy_commands.py
@@ -2,7 +2,8 @@
 This module contains the economy commands for the bot.
 """
 from discord.ext.commands import Cog, Context, hybrid_command
-from core.tools import send_bot_embed, economy_handler, retrieve_application_emoji
+from core.tools import send_bot_embed, economy_handler
+from controllers import update_user
 from config import MAX_SLOTS
 import random
 from collections import Counter
@@ -53,32 +54,7 @@ class EconomyCommands(Cog):
             await send_bot_embed(ctx, description="You do not have enough money to bet.")
             return
         
-        User.balance -= bet_amount
-        fruits = await self.get_fruits()
-        random_fruits = random.choices(fruits, k=MAX_SLOTS)
-        possible_jackpots = await self.get_jackpots()
-
-        title = f"{random_fruits}"
-        description = ""
-
-        fruits_freq = Counter(random_fruits)
-
-        if len(fruits_freq) == 1:
-            jackpot = possible_jackpots[random_fruits[0]]
-            User.balance += jackpot * bet_amount
-            description = f"You won the jackpot {jackpot}x your bet amount."
-
-        elif len(fruits_freq) == 2:
-            fruit = fruits_freq.most_common(1)[0][0]
-            fruit = fruit * 2
-            jackpot = possible_jackpots[fruit]
-            User.balance += jackpot * bet_amount
-            description = f"You won {jackpot}x your bet amount."
-
-        else:
-            description = "You lost."
-
-        await send_bot_embed(ctx, title=title, description=description)
+        await self.slots_handler(ctx, User, bet_amount)
              
     @hybrid_command(name="jackpots", aliases=["jp"], description="Check the jackpot values.")
     async def jackpots(self, ctx: Context) -> None:
@@ -91,10 +67,45 @@ class EconomyCommands(Cog):
         Returns:
             None
         """
-        candy_emoji = await retrieve_application_emoji("candy", 1295095109645373474, is_animated=True)
         jackpots = await self.get_jackpots()
-        jackpots_str = "\n".join([f"{key}: {value} {candy_emoji}" for key, value in jackpots.items()])
-        await send_bot_embed(ctx, title="Jackpots:", description=jackpots_str)
+        title = "🎰 Jackpots 🎰"
+        description = "``"
+        description += "\n".join([f"{emoji} = {value} 💸" for emoji, value in jackpots.items()])
+        description += "``"
+        footer_description = "All combinations that can be won in the slot machine, followed by how many times the bet amount you will win."
+        await send_bot_embed(ctx, title=title, footer_text=footer_description, description=description)
+
+    async def slots_handler(self, ctx: Context, User, bet_amount) -> None:
+        User.balance -= bet_amount
+        fruits = await self.get_fruits()
+        random_fruits = random.choices(fruits, k=MAX_SLOTS)
+
+        title = "🎰 Slot Machine 🎰"
+        row1 = "| {} | {} | {} |".format(*random.choices(fruits, k=3))
+        row2 = "| {} | {} | {} | <".format(*random_fruits)
+        row3 = "| {} | {} | {} |".format(*random.choices(fruits, k=3))
+        description = "```\n{}\n{}\n{}\n```".format(row1, row2, row3)
+
+        fruits_freq = Counter(random_fruits)
+        possible_jackpots = await self.get_jackpots()
+
+        if len(fruits_freq) == 1:
+            jackpot = possible_jackpots[random_fruits[0]]
+            User.balance += jackpot * bet_amount
+            description += f"\n🎉 **{ctx.author.display_name}** hit the jackpot! They won **{jackpot * bet_amount}**."
+
+        elif len(fruits_freq) == 2:
+            fruit = fruits_freq.most_common(1)[0][0]
+            fruit = fruit * 2
+            jackpot = possible_jackpots[fruit]
+            User.balance += jackpot * bet_amount
+            description += f"\n💰 **{ctx.author.display_name}** has won **{jackpot * bet_amount}**."
+
+        else:
+            description += f"\n😢 **{ctx.author.display_name}** has lost **{bet_amount}**."
+
+        if await update_user(User.id, balance=User.balance):
+            await send_bot_embed(ctx, title=title, description=description)
         
     async def get_jackpots(self) -> dict:
         """
@@ -136,6 +147,7 @@ class EconomyCommands(Cog):
             "🍊",
             "🍉"
         ]
+    
                  
 async def setup(bot):
     await bot.add_cog(EconomyCommands(bot))
