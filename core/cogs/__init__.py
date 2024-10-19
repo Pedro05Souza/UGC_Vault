2d0a32028aa93865cac3f6a9d4cf6d3fec63815e
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-18 23:48:59 -0300

    refactor: removal of error_handler for now

--- core/cogs/__init__.py
+++ core/cogs/__init__.py
@@ -1,6 +1,5 @@
 """
 The cogs package contains all the cogs that are loaded by the bot.
 """
-from .developer_commands import DeveloperCommands
-from .error_handler import ErrorHandler
-from .economy_commands import EconomyCommands
+from .developer_commands import *
+from .economy_commands import *
