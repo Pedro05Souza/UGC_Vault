05b7a8937eaa8b171f14f6970600d928dff066e5
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-19 21:32:16 -0300

    feat: timestamp functions

--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,11 +1,15 @@
-from models.user import User
+from models import User, CommandsTimestamp
 from typing import Optional
+from datetime import datetime
 
 __all__ = (
     'get_user',
     'create_user',
     'update_user',
     'get_user_balance',
+    'get_command_timestamp',
+    'create_command_timestamp',
+    'update_command_timestamp'
 )
 
 async def get_user(id: int) -> Optional[User]:
@@ -57,3 +61,48 @@ async def get_user_balance(id: int) -> Optional[int]:
     """
     user = await get_user(id)
     return user.balance if user else None
+
+async def get_command_timestamp(user_id: int, command_name: str) -> None:
+    """
+    Gets the timestamp of a command.
+
+    Args:
+        user_id (int): The user ID.
+        command_name (str): The command name.
+
+    Returns:
+        datetime: The timestamp.
+    """
+    querySet = await CommandsTimestamp.filter(user_id=user_id, command_name=command_name).first()
+
+    if not hasattr(querySet, 'timestamp'):
+        return None
+    
+    return querySet.timestamp
+
+async def create_command_timestamp(user_id: int, command_name: str) -> None:
+    """
+    Creates a command timestamp.
+
+    Args:
+        user_id (int): The user ID.
+        command_name (str): The command name.
+
+    Returns:
+        bool: Whether the command timestamp was created.
+    """
+    user = await get_user(user_id)
+    await CommandsTimestamp.create(user_id=user, command_name=command_name)
+
+async def update_command_timestamp(user_id: int, command_name: str) -> bool:
+    """
+    Updates a command timestamp.
+
+    Args:
+        user_id (int): The user ID.
+        command_name (str): The command name.
+
+    Returns:
+        bool: Whether the command timestamp was updated.
+    """
+    return await CommandsTimestamp.filter(user_id=user_id, command_name=command_name).update(timestamp=datetime.now())
