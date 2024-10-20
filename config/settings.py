0496448d56a0d0f705a03f1f6a3703ee58b8e214
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-19 21:31:18 -0300

    feat: add more constants

--- config/settings.py
+++ config/settings.py
@@ -11,3 +11,4 @@ ADMIN_IDS = {668666843900149791} # The IDs of the bot administrators (separator:
 # Command settings
 
 MAX_SLOTS = 3 # The maximum amount of slots that can be played at once
+MAX_COINFLIP = 4000 # The maximum amount that can be bet on a coinflip
