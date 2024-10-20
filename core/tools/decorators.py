9f3fab1ec202e25a629cf09d0844eb4f79bc2bda
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-19 21:34:24 -0300

    refactor: move bet commands to new module
    Reasoning:
    - The bet commands have been moved to a new module for better organization and separation of concerns.
    
    chore: replace int for big int
    
    Reasoning:
    - The use of `int` has been replaced with `big int` for handling larger numbers.
    
    feat: timestamp functions
    
    Reasoning:
    - Added new timestamp functions to enhance time-related functionality.
    
    feat: autocomplete
    
    Reasoning:
    - Implemented autocomplete feature for improved user experience.
    
    feat: guild controller
    
    Reasoning:
    - Added guild controller functionality for managing guild-related operations.

--- core/tools/decorators.py
+++ core/tools/decorators.py
@@ -2,9 +2,10 @@
 This module contains the decorators for the core of the application.
 """
 from config import CAN_LOG, ADMIN_IDS
-from core.tools.lib import send_bot_embed
+from core.tools.lib import send_user_dm, send_bot_embed, retrieve_application_emoji
 from discord.ext.commands import check
-from controllers import get_user, create_user
+from contextlib import suppress
+from controllers import get_user, create_user, get_guild, create_guild
 
 __all__ = (
     'economy_handler',
@@ -12,31 +13,46 @@ __all__ = (
     'admin_only',
 )
 
-def economy_handler(user_data = False, guild_config_data = False):
+def economy_handler(user_data=True, guild_data=True, booster_command = False):
     """
     Decorator that handles the economy data.
 
     Args:
         user_data (bool): Whether the function requires user data.
         guild_config_data (bool): Whether the function requires guild config data.
-
-        They will both be queried from the database and passed as arguments to the function.
+        booster_command (bool): Whether the function is a booster command.
 
     Returns:
         Decorator: The decorator.
     """
     async def predicate(ctx):
+
+        if guild_data:
+            guild_config = await get_guild(ctx.guild.id)
+
+            if not guild_config:
+                guild_config = await create_guild(ctx.guild.id)
+            
+            if ctx.channel.id not in guild_config.allowed_channels:
+                await send_user_dm(ctx.author, description=":no_entry_sign: You can't use this command in this channel.")
+                return False
+
         if user_data:
             user = await get_user(ctx.author.id)
 
             if not user:
                 user = await create_user(ctx.author.id)
-
+            
+            if booster_command:
+                if not ctx.author.premium_since:
+                    await send_bot_embed(ctx, description=f"{await retrieve_application_emoji(emoji_name='booster', emoji_id='1297349785442979881')} You must be a server booster to claim this reward.")
+                    return False
+            
             ctx.user_data = user
-
         return True
-    return check(predicate)
-
+    with suppress(Exception):
+        return check(predicate)
+    
 def check_logging():
     def wrapper(func):
         async def wrapped(*args, **kwargs):
