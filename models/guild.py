1a55ec2f28224fbd66ca6bd65afd810137afee63
Pedro Souza <pedroh.fsouzars@gmail.com>
2024-10-19 21:33:51 -0300

    chore: replace int for big int

--- models/guild.py
+++ models/guild.py
@@ -4,8 +4,8 @@ from tortoise import fields
 __all__ = ['Guilds']
 
 class Guilds(Model):
-    id = fields.IntField(primary_key=True)
-    allowed_channels = fields.JSONField()
+    id = fields.BigIntField(primary_key=True)
+    allowed_channels = fields.JSONField(null=True)
 
     def __str__(self):
         return f'guild_id: {self.id}, allowed_channels: {self.allowed_channels}'
