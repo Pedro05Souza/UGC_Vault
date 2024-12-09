b683af7141c956f59a87ff51f092becf098edf3e
Pedro <pedroh.fsouzars@gmail.com>
2024-12-08 22:36:28 -0300

    feat: enhance modals with item searcher and improved code formatting

--- core/views/add_code_modal.py
+++ core/views/add_code_modal.py
@@ -3,26 +3,32 @@ from discord import Interaction, TextStyle
 from core.tools import send_bot_embed
 from controllers import add_item_code
 
+
 class AddCodes(Modal, title="Add Codes"):
     def __init__(self, item_id: int, **kwargs):
         super().__init__(**kwargs)
         self.item_id = item_id
 
-        self.codes = TextInput (
+        self.codes = TextInput(
             label="Enter the codes you would like to add.",
             style=TextStyle.long,
-            placeholder="Enter the codes here, separated by a comma.",
+            placeholder="Enter the codes here, seperate them with a space.",
             required=True,
-            max_length=400
+            max_length=400,
         )
         self.add_item(self.codes)
 
     async def on_submit(self, interaction: Interaction) -> None:
         try:
-            codes = self.codes.value.split(",")
+            codes = self.codes.value.split()
             await add_item_code(self.item_id, codes)
-            await send_bot_embed(interaction, description="✅ The codes have been added successfully.", ephemeral=True)
+            await send_bot_embed(
+                interaction,
+                description="✅ The codes have been added successfully.",
+                ephemeral=True,
+            )
         except Exception as e:
             print(e)
-            await send_bot_embed(interaction, description=f"❌ An error occurred.", ephemeral=True)
-
+            await send_bot_embed(
+                interaction, description=f"❌ An error occurred.", ephemeral=True
+            )
--- core/views/change_price_modal.py
+++ core/views/change_price_modal.py
@@ -3,17 +3,18 @@ from discord import Interaction, TextStyle
 from core.tools import send_bot_embed
 from controllers import update_item_price
 
+
 class ChangePrice(Modal, title="Change Item Price"):
     def __init__(self, item_id: int, **kwargs):
         super().__init__(**kwargs)
         self.item_id = item_id
 
-        self.updated_price = TextInput (
+        self.updated_price = TextInput(
             label="Enter the price you would like to set.",
             style=TextStyle.short,
             placeholder="Enter the price here.",
             required=True,
-            max_length=100
+            max_length=100,
         )
         self.add_item(self.updated_price)
 
@@ -21,8 +22,13 @@ class ChangePrice(Modal, title="Change Item Price"):
         try:
             updated_price = int(self.updated_price.value)
             await update_item_price(self.item_id, updated_price)
-            await send_bot_embed(interaction, description="✅ The price has been updated sucessfully.", ephemeral=True)
+            await send_bot_embed(
+                interaction,
+                description="✅ The price has been updated sucessfully.",
+                ephemeral=True,
+            )
         except Exception as e:
             print(e)
-            await send_bot_embed(interaction, description=f"❌ An error occurred.", ephemeral=True)
-
+            await send_bot_embed(
+                interaction, description=f"❌ An error occurred.", ephemeral=True
+            )
