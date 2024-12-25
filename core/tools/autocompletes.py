2b796623b15e0e2155e358bd84c2c2505c6b23ff
Pedro <pedroh.fsouzars@gmail.com>
2024-12-25 16:50:00 -0300

    refact: ugc auto complete

--- core/tools/autocompletes.py
+++ core/tools/autocompletes.py
@@ -1,8 +1,7 @@
 from discord import Interaction, app_commands
-from repositories import get_all_items_with_codes
+from repositories import get_all_items_with_codes_and_quantity
 
-__all__ = ["color_autocomplete", "coinflip_autocomplete", "ugc_item_auto_complete"]
-
+__all__ = ["color_autocomplete", "ugc_item_auto_complete"]
 
 async def color_autocomplete(
     interaction: Interaction, current_choice: str
@@ -13,23 +12,16 @@ async def color_autocomplete(
         for color in colors
         if current_choice.lower() in color.lower()
     ]
-
-async def coinflip_autocomplete(
-    interaction: Interaction, current_choice: str
-) -> list[app_commands.Choice]:
-    sides = ["Heads", "Tails"]
-    return [
-        app_commands.Choice(name=side, value=side)
-        for side in sides
-        if current_choice.lower() in side.lower()
-    ]
     
 async def ugc_item_auto_complete(
     interaction: Interaction, current_choice: str
 ) -> list[app_commands.Choice]:
-    items = await get_all_items_with_codes()
+    items = await get_all_items_with_codes_and_quantity()
     return [
-        app_commands.Choice(name=item["item_name"], value=str(item["item_id"]))
+        app_commands.Choice(
+            name=f"{item["item_name"]}: {item["item_price"]} candies, 📦 stock: {len(item["codes__code"])}",
+            value=str(item["item_id"]))
         for item in items
         if current_choice.lower() in item["item_name"].lower()
     ]
+    
